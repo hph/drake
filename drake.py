1c4912af690df04fe7613f8305746336d9d1b6e8
Haukur Páll Hallvarðsson <haukurpallh@gmail.com>
2012-06-15 07:53:16 +0000

    Starting code - minor functionality

---
+++ drake.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+#coding=utf8
+
+# NOTES
+# PR hash:
+#os.urandom(16).encode('hex')
+
+import random
+import string
+
+
+CHAR_SETS = [string.lowercase, string.uppercase, string.digits,
+             string.punctuation, ' ']
+
+
+def generate_password(base=None, seed=None, length=16, char_sets=CHAR_SETS,
+                      min_objects=None, include=None, exclude=None):
+    '''Generate a password based on various options.'''
+    password = ''
+    if base:
+        length -= len(base[0])
+        base_string = base[0]
+        base_length = len(base_string)
+        base_alignment = base[1]
+    while len(password) < length:
+        if include:
+            char_sets[4] += include
+        # TODO See http://stackoverflow.com/questions/265960/best-way-to-strip-
+        # punctuation-from-a-string-in-python. Rewrite?
+        if exclude:
+            for char in exclude:
+                for i, char_set in enumerate(char_sets):
+                    if char in char_set:
+                        char_sets[i] = char_set.replace(char, '')
+        # TODO Add a max_objects argument.
+        if min_objects:
+            for i, item in enumerate(min_objects):
+                for _ in xrange(item):
+                    password += random.choice(char_sets[i])
+            # If the following two lines were not present the output would have
+            # an equal number of characters from each character set if the
+            # length of the generated password is a multiple of the sum of the
+            # items in min_objects.
+            extra_length = length - sum(min_objects) 
+            password += generate_password(length=extra_length)
+        else:
+            for _ in xrange(length):
+                password += random.choice(''.join(char_sets))
+    # Shuffle the characters if necessary (depends on the options).
+    password = ''.join(random.sample(password, length))
+    if base:
+        if base_alignment == 'left':
+            password = base_string + password
+        elif base_alignment == 'right':
+            password += base_string
+        # TODO Write something to change "example" into "$ex.am-2ple" or
+        # something similar (randomly select the lenght of each part).
+        elif base_alignment == 'split':
+            pass
+        elif base_alignment == 'random':
+            pos = random.randrange(0, length + base_length - 1)
+            password = password[:pos] + base_string + password[pos:]
+    return password
+
+
+def generate_wordlike_strings():
+    '''Used with generate_password() to generate an easier to remember
+    password.'''
+    pass
+
+
+def list_characters():
+    '''According to phonetic alphabet tables or common words.'''
+    pass
+
+
+def gauge_password_strength():
+    '''Gauge the strength of the input password. Output could be boolean,
+    numeric or verbose depending on the options.'''
+    pass
+
+
+def main():
+    # TODO Add argparse commands. Current code only for testing purposes.
+    print 'Example output with the default options: %s' % generate_password()
+
+
+if __name__ == '__main__':
+    main()
